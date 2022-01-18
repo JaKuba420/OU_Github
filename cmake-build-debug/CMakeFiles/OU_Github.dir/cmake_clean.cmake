cb7c8de7f4d068c8cb562722e3efe1bddf4d1757
JaKuba <240671@edu.p.lodz.pl>
2022-01-18 19:10:38 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_Github.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Github.dir/main.cpp.obj"
+  "OU_Github.exe"
+  "OU_Github.exe.manifest"
+  "OU_Github.pdb"
+  "libOU_Github.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_Github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
